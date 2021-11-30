720349f6c04191eadf02d8c0585670b3a5b055f1
NeoBeto10 <93846209+NeoBeto10@users.noreply.github.com>
2021-11-30 12:15:45 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -2,8 +2,34 @@
 
 //echo "Hola mundo!";
 
-//var_dump ($_SERVER);
+//var_dump($_SERVER);
 
-echo $_SERVER["REMOTE_ADDR"];
+//echo $_SERVER['REQUEST_METHOD'];
+$registro = [];
+
+/* array_push($registro, array('nombre' => 'Victor', 'apellido' => 'Pinedo'));
+array_push($registro, array('nombre' => 'David', 'apellido' => 'MNedina'));
+array_push($registro, array('nombre' => 'Kenia', 'apellido' => 'Vázquez'));
+
+echo json_encode($registro); */
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+
+        $nombre = $_GET['nombre'];
+        $apellido = $_GET['apellido'];
+        break;
+    case 'POST':
+
+        $nombre = $_POST['nombre'];
+        $apellido = $_POST['apellido'];
+        break;
+    default:
+        echo "El método HTTP no es reconocido";
+        break;
+}
+
+array_push($registro, array('nombre' => $nombre, 'apellido' => $apellido));
+echo json_encode($registro);
 
 ?>  
